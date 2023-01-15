df2b75e5455b59d750b93e8c104cd18e1fbe47e5
Imad Mimouni <imad.mimouni.123@gmail.com>
2023-01-15 23:39:37 +0100

    update

--- ft_minitalk.h
+++ ft_minitalk.h
@@ -6,7 +6,7 @@
 /*   By: imimouni <imimouni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 14:52:37 by imimouni          #+#    #+#             */
-/*   Updated: 2023/01/07 17:03:10 by imimouni         ###   ########.fr       */
+/*   Updated: 2023/01/15 23:38:05 by imimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 # include <signal.h>
 # include <unistd.h>
 # include "ft_printf/ft_printf.h"
+
+void	sig_error(void);
+void	tkmila(char *c, int *received, int *client_pid, int *bit);
+void	action(int sig, siginfo_t *info, void *context);
 
 # define RED "\033[0;31m"
 # define GREEN "\033[0;32m"
--- ft_server.c
+++ ft_server.c
@@ -6,32 +6,29 @@
 /*   By: imimouni <imimouni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 14:52:30 by imimouni          #+#    #+#             */
-/*   Updated: 2023/01/14 22:46:03 by imimouni         ###   ########.fr       */
+/*   Updated: 2023/01/15 23:37:05 by imimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minitalk.h"
 
-void	signal_error(void)
+int	main(void)
 {
-	ft_printf("\n%sserver: unexpected error.%s\n", RED, RESET);
-	exit(EXIT_FAILURE);
-}
+	int					pid;
+	struct sigaction	act;
 
-void	extended_action(char *c, int *received, int *client_pid, int *bit)
-{
-	ft_printf("%c", *c);
-	if (*c == '\0')
+	pid = getpid();
+	ft_printf("PID: %d\n", pid);
+	act.sa_sigaction = action;
+	sigemptyset(&act.sa_mask);
+	// act.sa_flags = SA_SIGINFO;
+	while (1)
 	{
-		ft_printf("\n%s%d signal recieved from client PID: %d%s\n",
-			GREEN, *received, *client_pid, RESET);
-		*received = 0;
-		*c = 0;
-		if (kill(*client_pid, SIGUSR1) == -1)
-			signal_error();
-		return ;
+		sigaction(SIGUSR1, &act, 0);
+		sigaction(SIGUSR2, &act, 0);
+		pause();
 	}
-	*bit = 0;
+	return (EXIT_FAILURE);
 }
 
 void	action(int sig, siginfo_t *info, void *context)
@@ -53,31 +50,37 @@ void	action(int sig, siginfo_t *info, void *context)
 		c = 0;
 		received = 0;
 	}
-	c |= (sig == SIGUSR2);
+	c = c | (sig == SIGUSR2);
+	// ft_printf("%d\n",c);
 	received++;
+	// ft_printf("%d\n",received);
 	bit++;
+	ft_printf("%d\n",bit);
 	if (bit == 8)
-		extended_action(&c, &received, &client_pid, &bit);
+		tkmila(&c, &received, &client_pid, &bit);
 	c <<= 1;
 	usleep(100);
 	kill(client_pid, SIGUSR2);
 }
 
-int	main(void)
+void	tkmila(char *c, int *received, int *client_pid, int *bit)
 {
-	int					pid;
-	struct sigaction	act;
-
-	pid = getpid();
-	ft_printf("PID: %d\n", pid);
-	act.sa_sigaction = action;
-	sigemptyset(&act.sa_mask);
-	// act.sa_flags = SA_SIGINFO;
-	while (1)
+	ft_printf("%c", *c);
+	if (*c == '\0')
 	{
-		sigaction(SIGUSR1, &act, 0);
-		sigaction(SIGUSR2, &act, 0);
-		pause();
+		ft_printf("\n%s%d signal recieved from client PID: %d%s\n",
+			GREEN, *received, *client_pid, RESET);
+		*received = 0;
+		*c = 0;
+		if (kill(*client_pid, SIGUSR1) == -1)
+			sig_error();
+		return ;
 	}
-	return (EXIT_FAILURE);
+	*bit = 0;
+}
+
+void	sig_error(void)
+{
+	ft_printf("\n%sserver: unexpected error.%s\n", RED, RESET);
+	exit(EXIT_FAILURE);
 }
